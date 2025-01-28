b8f9986faedaf02ba04e73b32bc1423626ebb343
آزومی <azumitan88@gmail.com>
2025-01-28 10:17:36 -0800

    Update lightweight.sh

--- lightweight.sh
+++ lightweight.sh
@@ -6,5 +6,5 @@ chmod +x /etc/logo.sh
 if [ -f "light_script.py" ]; then
     rm light_script.py
 fi
-wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/6to4/light_script.py
+wget https://github.com/Azumi87/6TO4-GRE-IPIP-SIT/releases/download/6to4/light_script.py
 python3 light_script.py
