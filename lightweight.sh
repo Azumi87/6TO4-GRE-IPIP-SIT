899e8c1e6b25cc00daa2c4eb564bda8a5651af15
آزومی <azumitan88@gmail.com>
2025-01-28 10:14:32 -0800

    Update lightweight.sh

--- lightweight.sh
+++ lightweight.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update -y
 apt install wget -y
-wget -O /etc/logo2.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
-chmod +x /etc/logo2.sh
+wget -O /etc/logo.sh https://github.com/Azumi87/6TO4-GRE-IPIP-SIT/blob/main/logo.sh
+chmod +x /etc/logo.sh
 if [ -f "light_script.py" ]; then
     rm light_script.py
 fi
-wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/ubuntu24/light_script.py
+wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/6to4/light_script.py
 python3 light_script.py
