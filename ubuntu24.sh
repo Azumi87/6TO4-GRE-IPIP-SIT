2d8e7b144005e189a820f600756007fb23b64504
آزومی <azumitan88@gmail.com>
2025-01-28 10:13:58 -0800

    Update ubuntu24.sh

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update -y
 apt install wget -y
-wget -O /etc/logo2.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
-chmod +x /etc/logo2.sh
+wget -O /etc/logo.sh https://github.com/Azumi87/6TO4-GRE-IPIP-SIT/blob/main/logo.sh
+chmod +x /etc/logo.sh
 if [ -f "ubuntu24.py" ]; then
     rm ubuntu24.py
 fi
-wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/ubuntu24/ubuntu24.py
+wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/6to4/ubuntu24.py
 python3 ubuntu24.py
